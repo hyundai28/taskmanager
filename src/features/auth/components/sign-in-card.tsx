868abdf55713ad2b28fee86a243faf90f72fb2c8
Victor Fazekas <victor.fazekas@hotmail.com>
2026-06-25 08:43:37 -0300

    commit from Hyundai

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,13 +1,8 @@
 "use client";
 
 import { z } from "zod";
-import Link from "next/link";
-import { FcGoogle } from "react-icons/fc";
-import { FaGithub } from "react-icons/fa";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-
-import { signUpWithGithub, signUpWithGoogle } from "@/lib/oauth";
 import { DottedSeparator } from "@/components/dotted-separator";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
@@ -22,6 +17,7 @@ import {
 
 import { loginSchema } from "../schemas";
 import { useLogin } from "../api/use-login";
+import Image from "next/image";
 
 export const SignInCard = () => {
   const { mutate, isPending } = useLogin();
@@ -40,8 +36,8 @@ export const SignInCard = () => {
 
   return (
     <Card className="w-full h-full md:w-121.75 border-none shadow-none">
-      <CardHeader className="flex items-center justify-center text-center p-7">
-        <CardTitle className="text-2xl">Welcome back!</CardTitle>
+      <CardHeader className="flex items-center justify-center text-center">
+         <Image src="/logo.png" alt="logo" width={200} height={128} />
       </CardHeader>
       <div className="px-7">
         <DottedSeparator />
@@ -83,37 +79,7 @@ export const SignInCard = () => {
         <DottedSeparator />
       </div>
       <CardContent className="p-7 flex flex-col gap-y-4">
-        <Button
-          onClick={() => signUpWithGoogle()}
-          disabled={isPending}
-          variant="secondary"
-          size="lg"
-          className="w-full"
-        >
-          <FcGoogle className="mr-2 size-5" />
-          Login with Google
-        </Button>
-        <Button
-          onClick={() => signUpWithGithub()}
-          disabled={isPending}
-          variant="secondary"
-          size="lg"
-          className="w-full"
-        >
-          <FaGithub className="mr-2 size-5" />
-          Login with Github
-        </Button>
-      </CardContent>
-      <div className="px-7">
-        <DottedSeparator />
-      </div>
-      <CardContent className="p-7 flex items-center justify-center">
-        <p>
-          Don&apos;t have an account?
-          <Link href="/sign-up">
-            <span className="text-blue-700">&nbsp;Sign Up</span>
-          </Link>
-        </p>
+        
       </CardContent>
     </Card>
   );
